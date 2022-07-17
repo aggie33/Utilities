dd8fd7c292fc1e59c2549f13436be58ca24ebbbc
aggie33 <aggie@startmail.com>
2022-07-17 10:31:19 -0400

    Workin'

--- Sources/Utilities/Utilities.swift
+++ Sources/Utilities/Utilities.swift
@@ -70,8 +70,125 @@ extension NSObjectProtocol where Self: NSObject {
         }
     }
 }
+
+@propertyWrapper public struct Copying<T: NSCopying> {
+    var _wrappedValue: T
+    
+    public var wrappedValue: T {
+        get {
+            _wrappedValue.copy() as! T
+        }
+        set {
+            _wrappedValue = newValue
+        }
+    }
+}
+
+@available(tvOS 13.0, iOS 13.0, watchOS 6.0, macOS 10.15, *)
+extension Color {
+    public static var random: Self {
+        Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1))
+    }
+}
+
 #endif
 
+func ?? <T> (lhs: T?, rhs: @autoclosure () -> Never) -> T {
+    if let lhs {
+        return lhs
+    } else {
+        rhs()
+    }
+}
 
-                
+prefix operator ++
+postfix operator ++
+
+public prefix func ++ <T: Numeric> (rhs: inout T) -> T {
+    rhs += 1
+    return rhs
+}
+
+public postfix func ++ <T: Numeric> (lhs: inout T) {
+    lhs += 1
+}
+
+extension Sequence where Element: AdditiveArithmetic {
+    public var sum: Element {
+        reduce(.zero, +)
+    }
+}
+
+extension Sequence where Element: Numeric {
+    public var product: Element {
+        reduce(1, *)
+    }
+}
+
+prefix operator +-
+
+public prefix func +- <T: SignedNumeric> (rhs: T) -> (T, T) {
+    return (rhs, -rhs)
+}
+
+// public typealias Result<Success, Failure> = Either<Success, Failure> where Failure: Error
+// public typealias Optional<Wrapped> = Either<Wrapped, Void>
+
+public enum Either<First, Second> {
+    case first(First)
+    case seecond(Second)
+    
+    var firstValue: First? {
+        if case let .first(first) = self {
+            return first
+        } else {
+            return nil
+        }
+    }
+    
+    var secondValue: Second? {
+        if case let .seecond(second) = self {
+            return second
+        } else {
+            return nil
+        }
+    }
+}
+
+prefix operator √
+
+public prefix func √ <T: BinaryFloatingPoint> (rhs: T) -> T {
+    rhs.squareRoot()
+}
+
+infix operator **
+
+public func ** (lhs: Double, rhs: Double) -> Double {
+    pow(lhs, rhs)
+}
+
+public func ** (lhs: Decimal, rhs: Int) -> Decimal {
+    pow(lhs, rhs)
+}
+
+public func ** <T: Numeric> (lhs: T, rhs: Int) -> T {
+    precondition(rhs >= 0, "Negative powers are not allowed")
+    if rhs == 0 { return 1 } else if rhs == 1 { return lhs }
+    
+    var value = lhs
+    for _ in 2...rhs {
+        value *= lhs
+    }
+    
+    return value
+}
+
+extension Result where Failure == Never {
+    public var value: Success {
+        switch self {
+        case let .success(success):
+            return success
+        }
+    }
+}
 
